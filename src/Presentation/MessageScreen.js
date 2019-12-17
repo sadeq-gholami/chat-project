d9d4135fc9c81cfbc4670ccf8a1956689d1e18ee
momahdi <mohamed4mahdi@gmail.com>
2019-12-17 11:27:38 +0100

    dummy

--- src/Presentation/MessageScreen.js
+++ src/Presentation/MessageScreen.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
-class Messagescreen extends Component {
-    state = {  }
+
+
+const hardCode= [
+    {
+        senderId :'Ulla',
+        text:'hej hur går det?' 
+    },
+    {
+        senderId :'Bergitta',
+        text:'Inte bra :(' 
+    },
+
+    {
+        senderId :'Eva',
+        text:'Tufft' 
+    }
+
+]
+
+
+
+
+class MessageScreen extends React.Component {
     render() { 
         return (  
-            <div></div>
+            <div className= "messagecreen" >
+                {hardCode.map((message,index)=>{
+                    return (
+                        <div key={index} className= "Message">
+                        <div className = "message-text"> {message.text}</div>
+                        <div className = "message-sender"> {message.senderId}</div>
+                        </div>
+                    )
+                })}
+                </div>
         );
     }
 }
  
-export default Messagecreen;
+export default MessageScreen;
