6f1ebae980213e41f9f818862028412b2d4e068c
zeinebAmri <zeinebamri13@gmail.com>
2019-12-17 15:21:49 +0100

    fixed some methods in model

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
-
-function App() {
-  return (
-      <div>Hello world</div>
-  );
+import ChatModel from './Model/ChatModel';
+import React, { Component } from 'react';
+//import Chatkit from '@pusher/chatkit';
+class App extends Component {
+  constructor(props){
+  super(props);
+  this.state = {  
+    model : new ChatModel()
+  }
 }
-
+  render() { 
+    this.state.model.init().then(
+      currentUser=> {this.state.model.subscribeToRoom("c1afc728-2acf-40d7-b0d5-f51ce79e2c04", currentUser)}
+    )
+    return ( 
+      <div></div>
+     );
+  }
+}
+ 
 export default App;
--- src/Model/ChatModel.js
+++ src/Model/ChatModel.js
@@ -1,6 +1,72 @@
-import React, { Component } from 'react';
-class ChatModel extends Component {
+import Chatkit from '@pusher/chatkit-client'
+import Observable from "./Observable";
+import {tokenUrl, instanceLocator} from "../config.js"
+
+
+export default class ChatModel extends Observable {
+   constructor(){
+       super();
+       this.messages =[];
+       this.currentRoomId= null;
+       this.currentUser = null;  //contains functions for interacting with the API.
+       this.availableRooms= [];
+       this.takenRooms= [];
+   }
     
+   init(){
+    const chatManager = new Chatkit.ChatManager({
+            instanceLocator,
+            userId: '8869',
+            tokenProvider: new Chatkit.TokenProvider({
+                url: tokenUrl
+        })
+
+    })
+    return chatManager.connect()
+    .then(currentUser => {
+        return currentUser
+        //return this.currentUser.getJoinableRooms()
+      /*  .then(availableRooms => {
+                this.availableRooms= availableRooms;
+                this.takenRooms= this.currentUser.rooms;
+            })*/
+        })           
+    .catch(err => console.log('Failed to connect: ', err))
 }
- 
-export default ChatModel;
+sendMessage(text) {
+    this.currentUser.sendMessage({
+        text,
+        roomId: this.state.currentRoomId
+    })
+}
+createRoom(name) {
+    this.currentUser.createRoom({
+        name
+    })
+    .then(room => this.subscribeToRoom(room.id))
+    .catch(err => console.log(err))
+
+}
+subscribeToRoom(roomId ,currentUser) {
+    this.messages= [];
+    currentUser.subscribeToRoom({
+        roomId: roomId,
+        hooks: {
+            onMessage: message => {
+                console.log("message.text:", message.text)
+                    //this.messages= [...this.state.messages, message]
+            }
+        }
+    })
+    .then(currentRoom => {
+        this.currentRoomId=  currentRoom.id});
+        return currentUser.getJoinableRooms()
+        .then(availableRooms => {
+            this.availableRooms = availableRooms;
+            this.takenRooms= currentUser.rooms;
+            })
+    .catch(err => console.log('error on subscribing: ', err))
+}
+}
+
+
