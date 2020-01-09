9085325f1e544f390756fe13d013156f77d241ba
Sadeq Gholami <sadeqg@kth.se>
2020-01-09 15:16:58 +0100

    fixed conflicts at home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -19,38 +19,30 @@ class Home extends Component {
     }
     render() { 
         return ( 
-           
-     <div className= "app1"> 
-         <div className= "outerbox">
-         <div className= "innerbox">
-        <div className ="imageholder">
-         <img  width="140" src={ require('../images/icon.png') } />
-         </div>
-          <form className ="username" onSubmit={this.handleSubmit}>
-            <input  onChange ={this.handlechange} 
-                    value={this.state.userName}
-                    placeholder="Enter username..."
-                    type="text"/>
-<<<<<<< HEAD
-            <div>
-            <Link to ={`/chatScreen/${this.state.userName.toLowerCase()}`}>
-                 <button id = "tbtn" onClick={e=>this.handleSubmit(e)} 
-                 type="button" >Sign in
-                 </button> 
-                
-            </Link>
-             </div>
-=======
-            <button id = "Sbtn" onClick={e=>this.handleSubmit(e)} type="button" >
-                <Link to ={`/chatScreen/${this.state.userName.toLowerCase()}`}>login</Link>
-                </button>
->>>>>>> f2715e923f7f88bd0aa09a9144ac205221a0a8dd
-            </form>
+            <div className= "app1"> 
+                <div className= "outerbox">
+                    <div className= "innerbox">
+                        <div className ="imageholder">
+                            <img  width="140" src={ require('../images/icon.png') } />
+                        </div>
+                        <form className ="username" onSubmit={this.handleSubmit}>
+                            <input  onChange ={this.handlechange} 
+                                value={this.state.userName}
+                                placeholder="Enter username..."
+                                type="text"/>
+                            <div>
+                                <Link to ={`/chatScreen/${this.state.userName.toLowerCase()}`}>
+                                    <button id = "tbtn" onClick={e=>this.handleSubmit(e)} 
+                                        type="button">Sign in
+                                    </button> 
+                        
+                                </Link>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-              </div>
-            </div>
-            
-         );
+            </div>  
+        );
     }
 }
  
