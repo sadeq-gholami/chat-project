7ad411f46f777a400809d88321c881d6b3e833dc
zeineb <zeinebAmri@github.com>
2020-01-30 16:07:13 +0100

    cleaned header

--- src/Presentation/Header.js
+++ src/Presentation/Header.js
@@ -10,30 +10,28 @@ class Header extends Component {
         return ( 
            
         <div className="header">
-                    <div className="title"> 
-                    {this.props.currentroomName} 
+            <div className="title"> 
+                {this.props.currentroomName} 
                     <div className="settingicon">
-                    <img  width="30" className={"settingicon"}
-                        src ={ require('../images/settingicon.png')}
-                        alt ={"could not load"}
-                        onClick={this.props.displaySettings}/>
+                        <img  width="30" className={"settingicon"}
+                            src ={ require('../images/settingicon.png')}
+                            alt ={"could not load"}
+                            onClick={this.props.displaySettings}/>
                         <span className="tooltiptext">Settings</span>
-                        </div>
+                    </div>
                     <div className="settingicon2">
                         <img width="30" className={"settingicon"}
                              src={require('../images/users.png')}
                              alt={"could not load"}
                              onClick={this.props.displayRoomSettings}/>
                     </div>
-
                     <div className="createinvicon">
-                            <img  width="28" className={"createinvicon"}
+                        <img  width="28" className={"createinvicon"}
                             src ={ require('../images/createinvicon.png')}
                             alt ={"could not load"}
                             onClick={this.props.displayPopupInvite}/>
-                            <span className="tooltiptext">Create invite</span>
-                            </div>
-
+                        <span className="tooltiptext">Create invite</span>
+                    </div>
                     <Link to={"/photos"}>
                         <div  className="allpicture">   
                         <img  width="40" src={ require('../images/photos.png')} alt={"could not load"} />
@@ -42,8 +40,8 @@ class Header extends Component {
                     </Link>
 
                     <div className="tabicon">
-                    <img  width="30" src={ require('../images/tabicon.png') } 
-                        onClick={this.props.collapseSidebar}/>
+                        <img  width="30" src={ require('../images/tabicon.png') } 
+                            onClick={this.props.collapseSidebar}/>
                     </div>
                 </div>
             </div>
